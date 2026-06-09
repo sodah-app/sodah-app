5724a4d688b58f70fbf2ebac1422a4070c78398b
Solomon dagbah <Solomondagbahz@gmail.com>
2026-06-09 10:21:39 +0400

    Fix connect whatsapp build

--- app/connect-whatsapp/page.js
+++ app/connect-whatsapp/page.js
@@ -1,11 +1,12 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useEffect, useState } from "react";
 import {
   useRouter,
   useSearchParams
 } from "next/navigation";
-
 export default function ConnectWhatsAppPage() {
   const router = useRouter();
 
